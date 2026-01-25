c1d7505dda4e923f878b08191f6c16cab06c4746
maccha-on <masato.ohtani.tech0@gmail.com>
2026-01-25 16:55:31 +0900

    コード直打ちでバックエンド指定します。

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -8,10 +8,8 @@
 // export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
 //
 // ★26.01.24 修正 maccha
-// 上記の渡し方だとビルドで固定されてしまうため、localhostの記載を削除します。
-// ローカル対応したい場合は、次のように修正してください。
-// (A) Next.js 側に /api/config を作って、サーバ側で env を読んで返す（クライアントはそれを fetch）
-export const BACKEND_API_URL = process.env.NEXT_PUBLIC_API_URL;
+// 上記の渡し方だとビルドで固定されてしまていたため、一旦localhostの記載を削除します。
+export const BACKEND_API_URL = 'https://tech0-gen-11-step3-2-node-55.azurewebsites.net'
 
 // アプリケーション名
 export const APP_NAME = "司書AIアプリ MOF";

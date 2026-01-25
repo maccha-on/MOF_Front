ddbda62a78b803dc0f32386a6406b608c7c08346
maccha-on <masato.ohtani.tech0@gmail.com>
2026-01-25 17:25:24 +0900

    ベタ打ち修正

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -9,7 +9,7 @@
 //
 // ★26.01.24 修正 maccha
 // 上記の渡し方だとビルドで固定されてしまていたため、一旦localhostの記載を削除します。
-export const BACKEND_API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const BACKEND_API_URL = 'https://tech0-gen-11-step3-2-py-52.azurewebsites.net';
 
 // アプリケーション名
 export const APP_NAME = "司書AIアプリ MOF";
